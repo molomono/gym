ef7e43972760606772ac4544922f5e6aefe8a784
molomono <molomono@users.noreply.github.com>
2019-03-20 13:21:46 +0100

    Added example V-Rep Environments

---
+++ gym/envs/vrep/__init__.py
@@ -0,0 +1,3 @@
+from gym.envs.vrep.cartpole_vrep_env import CartPoleVrepEnv
+from gym.envs.vrep.cartpole_continuous_vrep_env import CartPoleContinuousVrepEnv
+from gym.envs.vrep.hopper_vrep_env import HopperVrepEnv
